7b07ac7e81fa32d62e748ba3da587a96b1e203ad
shivam21bb106 <shivam21bbbb@gmail.com>
2025-09-25 21:05:43 +0530

    search functionality

--- cryptoscreener/src/components/Search.jsx
+++ cryptoscreener/src/components/Search.jsx
@@ -7,6 +7,8 @@ export default function Search() {
 
     const [searchText, setSearchText] = useState('')
     let { getSearchResult } = useContext(CryptoContext)
+    let { searchData, setCoinSearch, setSearchData } = useContext(CryptoContext)
+    console.log("SEEEEEEEEEAAARCHHHHHHH", searchData)
     let handleInput = (e) => {
 
         e.preventDefault();
@@ -15,28 +17,46 @@ export default function Search() {
         getSearchResult(query)
     }
 
+    const selectCoin = (coin) => {
+        setCoinSearch(coin)
+        setSearchText('')
+        setSearchData('')
+    }
     return (<>
         <form className="w-96 relative flex items-center ml-7">
-            <input type="text" name="search"
-                className="w-full rounded bg-gray-200 
-            placeholder:text-gray-100 pl-2 required outline-0 border border-transparent focus:border-cyan"
+            <input
+                type="text"
+                name="search"
+                className="w-full rounded bg-blue-200 
+      placeholder:text-gray-400 pl-2 outline-0 
+      border border-transparent focus:border-cyan-500"
                 placeholder="Search here"
                 onChange={handleInput}
                 value={searchText}
             />
-            <button type="submit" className="absolute right-1 cursor-pointer">
-                <img src={searchIcon} alt="search" className="w-full h-auto" />
-            </button>
         </form>
+
         {
 
 
 
             searchText.length > 0 ?
-                <ul className="absolute top-11 right-0 w-96 h-96 rounded overflow-x-hidden py-2 bg-gray-200 bg-opacity-60 
-                backdrop-blur-md">
-                    <li>Bitcoin</li>
-                    <li>etherium</li>
+                <ul className="absolute top-11 left-0 w-96 max-h-96 overflow-auto rounded py-2 bg-black bg-opacity-60 backdrop-blur-md mx-6">
+
+                    {
+
+                        searchData.length > 0 ? searchData.map(coin => {
+                            return <li className="flex items-center ml-4 my-2 cursor-pointer"
+                                key={coin.id} onClick={() => selectCoin(coin.id)}>
+                                <img
+                                    className="w-[1rem] h-[1rem] mx-1.5"
+                                    src={coin.thumb}
+                                    alt={coin.name}
+                                />
+                                <span>{coin.name}</span>
+                            </li>
+                        }) : <h2>Please Wait</h2>
+                    }
 
                 </ul>
 
--- cryptoscreener/src/context/CryptoContext.jsx
+++ cryptoscreener/src/context/CryptoContext.jsx
@@ -1,28 +1,21 @@
 import { createContext, useLayoutEffect, useState } from "react";
 
-
-
-
-
-
-
-
-
-
-
-
 export const CryptoContext = createContext({});
 export const CryptoProvider = ({ children }) => {
     const [cryptoData, setCryptoData] = useState();
-    const [searchData, setSearchData] = useState('')
+    const [searchData, setSearchData] = useState([])
+    const [coinSearch, setCoinSearch] = useState('')
+
+
+
 
     const getCryptoData = async () => {
         try {
             const data = await fetch(
-                "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d"
-            ).then(res => res.json()).then(json => json);
+                `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=${coinSearch}&order=market_cap_desc&per_page=10&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d`).then(res => res.json()).then(json => json);
 
             setCryptoData(data);
+            console.log(data)
         } catch (error) {
             console.log("Error fetching crypto data:", error);
         }
@@ -35,7 +28,7 @@ export const CryptoProvider = ({ children }) => {
                 `https://api.coingecko.com/api/v3/search?query=${query}`
             ).then(res => res.json()).then(json => json);
             console.log(data)
-            setCryptoData(data);
+            setSearchData(Array.isArray(data?.coins) ? data.coins : []);
         } catch (error) {
             console.log("Error fetching crypto data:", error);
         }
@@ -43,9 +36,9 @@ export const CryptoProvider = ({ children }) => {
 
     useLayoutEffect(() => {
         getCryptoData();
-    }, [])
+    }, [coinSearch])
     return (
-        <CryptoContext.Provider value={{ cryptoData, searchData, getSearchResult }}>
+        <CryptoContext.Provider value={{ cryptoData, searchData, getSearchResult, setCoinSearch, setSearchData }}>
             {children}
         </CryptoContext.Provider>
 
